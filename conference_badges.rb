7cfbb5683a31cfc98c577cd102f6388629fb8295
Mnkith <munkithabid@gmail.com>
2020-10-23 01:28:49 -0500

    commit tree 4abb9f76166daf0529b7faafe8585aa6f67a3b80

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator speakers
+  speakers.collect { |speaker| badge_maker speaker}
+end
+
+def assign_rooms speakers
+  room_number = 0
+  speakers.collect { |speaker| "Hello, #{speaker}! You'll be assigned to room #{room_number += 1}!"}
+end
+
+def printer speakers
+  batch_badge_creator(speakers).each { |badge| puts badge}
+  assign_rooms(speakers).each { |room| puts room}
+end
+  
